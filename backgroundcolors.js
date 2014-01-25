ce023d82160f456fc062c0be3a19f44a9f631193
NicFitzzz <neiltsalazar@gmail.com>
2014-01-24 23:18:51 -0800

    larger sizes added
    added larger sizes and cleaned up css and js a little bit

--- backgroundcolors.js
+++ backgroundcolors.js
@@ -4,25 +4,21 @@ $(document).ready(function () {
                   function () {
                       $('body').css("background", "#c44500");
                       $('#home').css("background", "#c44500");
-                      $('#home').css("color", "#fff");
-                  },
+                  }
                   function () {
                       $('body').css("background", "#c44500");
                       $('#home').css("background", "#fff");
-                      $('#home').css("color", "#000");
                   }
                 );
 
                 $("#folio").hover(
                   function () {
                       $('body').css("background", "#000");
-                      $('#folio').css("background", "#000");
-                      $('#folio').css("color", "#fff");
-                  },
+                      $('#folio').css("background", "red");
+                  } 
                   function () {
                       $('body').css("background", "#c44500");
                       $('#folio').css("background", "#fff");
-                      $('#folio').css("color", "#000");
                   }
                 );
 
@@ -30,12 +26,10 @@ $(document).ready(function () {
                   function () {
                       $('body').css("background", "#0058c4");
                       $('#about').css("background", "#0058c4");
-                      $('#about').css("color", "#fff");
-                  }, 
+                  } 
                   function () {
                       $('body').css("background", "#c44500");
                       $('#about').css("background", "#fff");
-                      $('#about').css("color", "#000");
                   }
                 );
 
@@ -43,12 +37,11 @@ $(document).ready(function () {
                   function () {
                       $('body').css("background", "#00c498");
                       $('#contact').css("background", "#00c498");
-                      $('#contact').css("color", "#fff");
-                  }, 
+
+                  } 
                   function () {
                       $('body').css("background", "#c44500");
                       $('#contact').css("background", "#fff");
-                      $('#contact').css("color", "#000");
                   }
                 );
             });
